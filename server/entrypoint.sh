34dbfe66d670e1ae95cbb6561d0e50eea70be356
WillTDA <62032600+WillTDA@users.noreply.github.com>
2026-03-22 02:03:36 +0000

    Optimise docker scripts

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Create uploads directory if it doesn't exist (runs after volume mount)
+# Create uploads directory if it doesn't exist then drop to dropgate
 mkdir -p /usr/src/app/uploads/db
 chown -R dropgate:dropgate /usr/src/app/uploads
 exec su-exec dropgate "$@"
